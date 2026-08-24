ce768ab6cb11ba72705996bd433e29e527fb0240
choice21 <storageusmanau@gmail.com>
2026-08-24 19:09:14 -0400

    fix: apply async skin fetch results on server thread

--- bukkit/src/main/java/io/github/gonalez/znpcs/skin/ApplySkinFetcherListener.java
+++ bukkit/src/main/java/io/github/gonalez/znpcs/skin/ApplySkinFetcherListener.java
@@ -3,6 +3,7 @@ package io.github.gonalez.znpcs.skin;
 import com.google.common.base.Preconditions;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import io.github.gonalez.znpcs.ServersNPC;
 import io.github.gonalez.znpcs.npc.NPC;
 import io.github.gonalez.znpcs.npc.NPCSkin;
 
@@ -17,8 +18,9 @@ public abstract class ApplySkinFetcherListener implements SkinFetcherListener {
   public void onComplete(GameProfile gameProfile) {
     Property textureProperty = GameProfiles.getTextureProperty(gameProfile);
     if (textureProperty != null) {
-      npc.changeSkin(NPCSkin.forValues(
-          textureProperty.getValue(), textureProperty.getSignature()));
+      NPCSkin skin = NPCSkin.forValues(textureProperty.getValue(), textureProperty.getSignature());
+      // Skin/profile retrieval runs off-thread; modifying viewers and sending packets must not.
+      ServersNPC.SCHEDULER.runTask(() -> npc.changeSkin(skin));
     }
   }
 }
