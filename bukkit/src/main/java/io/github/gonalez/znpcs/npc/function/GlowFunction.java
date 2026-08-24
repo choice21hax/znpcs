4a3e2101b45250f80c9e03056217150a7840e600
choice21 <storageusmanau@gmail.com>
2026-08-24 19:02:07 -0400

    modern: make glow function PacketEvents-safe on 26.x

--- bukkit/src/main/java/io/github/gonalez/znpcs/npc/function/GlowFunction.java
+++ bukkit/src/main/java/io/github/gonalez/znpcs/npc/function/GlowFunction.java
@@ -1,6 +1,7 @@
 package io.github.gonalez.znpcs.npc.function;
 
 import io.github.gonalez.znpcs.cache.CacheRegistry;
+import io.github.gonalez.znpcs.modern.ModernPacketBridge;
 import io.github.gonalez.znpcs.npc.FunctionContext;
 import io.github.gonalez.znpcs.npc.FunctionFactory;
 import io.github.gonalez.znpcs.npc.NPC;
@@ -13,24 +14,30 @@ public class GlowFunction extends NPCFunction {
 
   protected ResultType runFunction(NPC npc, FunctionContext functionContext) {
     if (!(functionContext instanceof FunctionContext.ContextWithValue)) {
-      throw new IllegalStateException("invalid context type, " + functionContext.getClass().getSimpleName() + ", expected ContextWithValue.");
+      throw new IllegalStateException("invalid context type, " + functionContext.getClass().getSimpleName()
+          + ", expected ContextWithValue.");
     }
     final String glowColorName = ((FunctionContext.ContextWithValue) functionContext).getValue();
+
+    if (ModernPacketBridge.isModern()) {
+      npc.getNpcPojo().setGlowName(
+          glowColorName == null || glowColorName.isEmpty() ? "WHITE" : glowColorName.toUpperCase());
+      npc.setGlowColor(null);
+      npc.deleteViewers();
+      return ResultType.SUCCESS;
+    }
+
     try {
-      // find the glow color enum
       final Object glowColor = CacheRegistry.ENUM_CHAT_FORMAT_FIND.load().invoke(null,
           glowColorName == null || glowColorName.length() == 0 ? "WHITE" : glowColorName);
-      if (glowColor == null)
-        return ResultType.FAIL;
+      if (glowColor == null) return ResultType.FAIL;
       npc.getNpcPojo().setGlowName(glowColorName);
       npc.setGlowColor(glowColor);
       CacheRegistry.SET_DATA_WATCHER_METHOD.load().invoke(
           CacheRegistry.GET_DATA_WATCHER_METHOD.load().invoke(npc.getNmsEntity()),
           CacheRegistry.DATA_WATCHER_OBJECT_CONSTRUCTOR.load().newInstance(
-              0,
-              CacheRegistry.DATA_WATCHER_REGISTER_FIELD.load()),
+              0, CacheRegistry.DATA_WATCHER_REGISTER_FIELD.load()),
           !FunctionFactory.isTrue(npc, this) ? (byte) 0x40 : (byte) 0x0);
-      // update glow scoreboard packets
       npc.getPackets().getProxyInstance().update(npc.getPackets());
       npc.deleteViewers();
       return ResultType.SUCCESS;
@@ -38,12 +45,15 @@ public class GlowFunction extends NPCFunction {
       return ResultType.FAIL;
     }
   }
-  
+
   protected boolean allow(NPC npc) {
-    return npc.getPackets().getProxyInstance().allowGlowColor();
+    return ModernPacketBridge.isModern() || npc.getPackets().getProxyInstance().allowGlowColor();
   }
-  
+
   public NPCFunction.ResultType resolve(NPC npc) {
+    if (ModernPacketBridge.isModern()) {
+      return ResultType.SUCCESS;
+    }
     return runFunction(npc, new FunctionContext.ContextWithValue(npc, npc.getNpcPojo().getGlowName()));
   }
 }
